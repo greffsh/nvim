65068dbfbadbd39c53510e8582d43651f942e744
greffsh <vgreffdias@gmail.com>
2026-07-03 02:36:34 -0300

    telescope bindings removed

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,9 +4,9 @@ return {
 		tag = "0.1.6",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>f", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>/", builtin.live_grep, {})
+			-- local builtin = require("telescope.builtin")
+			-- vim.keymap.set("n", "<leader>f", builtin.find_files, {})
+			-- vim.keymap.set("n", "<leader>/", builtin.live_grep, {})
 		end,
 	},
 	{
