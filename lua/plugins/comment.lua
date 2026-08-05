fb0215e617a397dca6c2299fda603ad35dfa2e72
greffsh <vgreffdias@gmail.com>
2026-08-04 21:56:20 -0300

    feat: gc binding on env files

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,5 +1,12 @@
 return {
 	"numToStr/Comment.nvim",
-	opts = {},
+	opts = {
+		-- Comment.nvim does not have a built-in entry for Neovim's `env` filetype.
+		pre_hook = function()
+			if vim.bo.filetype == "env" then
+				return "# %s"
+			end
+		end,
+	},
 	lazy = false,
 }
