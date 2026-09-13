0162d54c37b4f5fdb461833de53d9a701921e7d7
greffsh <vgreffdias@gmail.com>
2026-09-13 18:06:20 -0300

    feat(telescope): open non staged files on telescope finder

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,16 +4,29 @@ return {
 		tag = "0.1.6",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
-			-- local builtin = require("telescope.builtin")
-			-- vim.keymap.set("n", "<leader>f", builtin.find_files, {})
-			-- vim.keymap.set("n", "<leader>/", builtin.live_grep, {})
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>gs", builtin.git_status, {
+				desc = "Git changed files",
+			})
 		end,
 	},
 	{
 		"nvim-telescope/telescope-ui-select.nvim",
 		config = function()
+			local actions = require("telescope.actions")
+
 			require("telescope").setup({
 				defaults = {
+					mappings = {
+						i = {
+							["<C-q>"] = false,
+							["<C-e>"] = actions.send_to_qflist + actions.open_qflist,
+						},
+						n = {
+							["<C-q>"] = false,
+							["<C-e>"] = actions.send_to_qflist + actions.open_qflist,
+						},
+					},
 					layout_config = {
 						horizontal = {
 							preview_width = 0.6,
