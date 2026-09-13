788dd627f143848098a60e0d557bd6b6bedd5012
greffsh <vgreffdias@gmail.com>
2026-09-13 18:06:03 -0300

    feat: gitsigns

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,37 @@
+return {
+	"lewis6991/gitsigns.nvim",
+	opts = {
+		signs = {
+			add = { text = "+" },
+			change = { text = "~" },
+			delete = { text = "_" },
+			topdelete = { text = "‾" },
+			changedelete = { text = "~" },
+			untracked = { text = "┆" },
+		},
+		on_attach = function(bufnr)
+			local gitsigns = require("gitsigns")
+			local function map(lhs, rhs, desc)
+				vim.keymap.set("n", lhs, rhs, { buffer = bufnr, desc = desc })
+			end
+
+			map("]h", function()
+				if vim.wo.diff then
+					vim.cmd.normal({ "]c", bang = true })
+				else
+					gitsigns.nav_hunk("next")
+				end
+			end, "Next Git hunk")
+
+			map("[h", function()
+				if vim.wo.diff then
+					vim.cmd.normal({ "[c", bang = true })
+				else
+					gitsigns.nav_hunk("prev")
+				end
+			end, "Previous Git hunk")
+
+			map("<leader>hp", gitsigns.preview_hunk, "Preview Git hunk")
+		end,
+	},
+}
